6c64f37ee653413e8df54cf050e95108a5e644e9
lucasveronez <lucasveronez.dev@gmail.com>
2025-01-01 20:00:06 -0300

    Em start adicionado matrizes e foreach para todos os alunos usando foreach e if-else

--- GuidedProject/Starter/Program.cs
+++ GuidedProject/Starter/Program.cs
@@ -8,20 +8,45 @@ int[] andrewScores = new int[5] { 92, 89, 81, 96, 90 };
 int[] emmaScores = new int[5] { 90, 85, 87, 98, 68 };
 int[] loganScores = new int[5] { 90, 95, 87, 88, 96 };
 
-int sophiaSum = 0;
-
-decimal sophiaScore;
-
-foreach (int score in sophiaScores)
-{
-    // add the exam score to the sum
-    sophiaSum += score;    
-}
-
-sophiaScore = (decimal)sophiaSum / currentAssignments;
+// Student names
+string[] studentNames = new string[4] { "Sophia", "Andrew", "Emma", "Logan" };
+int[] studentScores = new int[10];
 
 Console.WriteLine("Student\t\tGrade\n");
-Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+
+foreach (string name in studentNames)
+{
+    string currentStudent = name;
+
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
+
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+
+    // initialize/reset the sum of scored assignments
+    int sumAssignmentScores = 0;
+    
+    // initialize/reset the calculated average of exam + extra credit scores
+    decimal currentStudentGrade = 0;
+
+    foreach (int score in studentScores)
+    {
+        // add the exam score to the sum
+        sumAssignmentScores += score;
+    }
+
+    currentStudentGrade = (decimal)(sumAssignmentScores) / currentAssignments;
+
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t?");
+}
+
 
 
 Console.WriteLine("Press the Enter key to continue");
