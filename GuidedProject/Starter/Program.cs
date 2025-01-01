284fc1d929169a0bdd195d8001e37385c1fc2afb
lucasveronez <lucasveronez.dev@gmail.com>
2025-01-01 11:08:31 -0300

    Em start adicionado matrizes e foreach para sophia

--- GuidedProject/Starter/Program.cs
+++ GuidedProject/Starter/Program.cs
@@ -3,67 +3,26 @@
 // initialize variables - graded assignments 
 int currentAssignments = 5;
 
-int sophia1 = 90;
-int sophia2 = 86;
-int sophia3 = 87;
-int sophia4 = 98;
-int sophia5 = 100;
-
-int andrew1 = 92;
-int andrew2 = 89;
-int andrew3 = 81;
-int andrew4 = 96;
-int andrew5 = 90;
-
-int emma1 = 90;
-int emma2 = 85;
-int emma3 = 87;
-int emma4 = 98;
-int emma5 = 68;
-
-int logan1 = 90;
-int logan2 = 95;
-int logan3 = 87;
-int logan4 = 88;
-int logan5 = 96;
-
-int[] storeNotesSophia = new int[5] { sophia1, sophia2, sophia3, sophia4, sophia5 };
-int i = 0;
-foreach (int note in storeNotesSophia)
-{
-    i += note;
-}
-System.Console.WriteLine($"A soma das notas de Sophia é: {i}");
-
-int[] storeNotesAndrew = new int[5] { andrew1, andrew2, andrew3, andrew4, andrew5 };
-int[] storeNotesEmma = new int[5] { emma1, emma2, emma3, emma4, emma5 };
-int[] storeNotesLogan = new int[5] { logan1, logan2, logan3, logan4, logan5 };
+int[] sophiaScores = new int[5] { 90, 86, 87, 98, 100 };
+int[] andrewScores = new int[5] { 92, 89, 81, 96, 90 };
+int[] emmaScores = new int[5] { 90, 85, 87, 98, 68 };
+int[] loganScores = new int[5] { 90, 95, 87, 88, 96 };
 
 int sophiaSum = 0;
-int andrewSum = 0;
-int emmaSum = 0;
-int loganSum = 0;
 
 decimal sophiaScore;
-decimal andrewScore;
-decimal emmaScore;
-decimal loganScore;
 
-sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
-andrewSum = andrew1 + andrew2 + andrew3 + andrew4 + andrew5;
-emmaSum = emma1 + emma2 + emma3 + emma4 + emma5;
-loganSum = logan1 + logan2 + logan3 + logan4 + logan5;
+foreach (int score in sophiaScores)
+{
+    // add the exam score to the sum
+    sophiaSum += score;    
+}
 
 sophiaScore = (decimal)sophiaSum / currentAssignments;
-andrewScore = (decimal)andrewSum / currentAssignments;
-emmaScore = (decimal)emmaSum / currentAssignments;
-loganScore = (decimal)loganSum / currentAssignments;
 
 Console.WriteLine("Student\t\tGrade\n");
 Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
-Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
-Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
-Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
+
 
 Console.WriteLine("Press the Enter key to continue");
 Console.ReadLine();
